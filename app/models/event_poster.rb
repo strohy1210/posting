aea4aba1e932a54b2652a0d55e7b224b648092e6
strohy120 <sarah.stroh@flatironschool.com>
2015-02-11 18:11:06 -0500

    YESS working tweet method

--- app/models/event_poster.rb
+++ app/models/event_poster.rb
@@ -1,9 +1,18 @@
-class EventPoster < ActiveRecord::Base
+class EventPoster
+
+  def self.post_events
+    poster = EventPoster.new
+    poster.tweet
+    Event.where(posted: false).each do |event|
+      event.update(posted: true)
+    end #assumes all posts are successful- although bad input may inhibit post ability
+  end
 
   def tweet
-    binding.pry
-
-
+    new_events = Event.where(posted: false)
+    new_events.all.each do |event|
+      $twitter.update("#{event.title} happening on #{event.date} at #{event.address} - don't miss out!")
+    end
   end
 
 
